88be674f8cced5c6b701d89bdebe3ea6db11bc05
rajjaiswal07 <rajjaiswal07.rj@gmail.com>
2021-09-27 11:11:42 +0530

    fragment

--- app/src/main/java/com/example/newsapp/EntertainmentFragment.java
+++ app/src/main/java/com/example/newsapp/EntertainmentFragment.java
@@ -8,15 +8,56 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class EntertainmentFragment extends Fragment {
+
+    String api = "fbcbec0e640749a891192b254d6d28e9";
+    ArrayList<ModelClass> modelClassArrayList;
+    Adapter adapter;
+    String country = "in";
+    private RecyclerView recyclerViewofentertainment;
+    private String category = "entertainment";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.entertainmentfragment, null);
+
+        recyclerViewofentertainment = v.findViewById(R.id.recyclerviewofentertainment);
+        modelClassArrayList = new ArrayList<>();
+        recyclerViewofentertainment.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new Adapter(getContext(), modelClassArrayList);
+        recyclerViewofentertainment.setAdapter(adapter);
+        findNews();
         return v;
+
+    }
+
+    private void findNews() {
+
+        ApiUtilities.getApiInterFace().getCategoryNews(country, category, 100, api).enqueue(new Callback<mainNews>() {
+            @Override
+            public void onResponse(Call<mainNews> call, Response<mainNews> response) {
+                if(response.isSuccessful()) {
+                    modelClassArrayList.addAll(response.body().getArticles());
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<mainNews> call, Throwable t) {
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/newsapp/HealthFragment.java
+++ app/src/main/java/com/example/newsapp/HealthFragment.java
@@ -8,15 +8,56 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HealthFragment extends Fragment {
+
+    String api = "fbcbec0e640749a891192b254d6d28e9";
+    ArrayList<ModelClass> modelClassArrayList;
+    Adapter adapter;
+    String country = "in";
+    private RecyclerView recyclerViewofhealth;
+    private String category = "health";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.healthfragment, null);
+
+        recyclerViewofhealth = v.findViewById(R.id.recyclerviewofhealth);
+        modelClassArrayList = new ArrayList<>();
+        recyclerViewofhealth.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new Adapter(getContext(), modelClassArrayList);
+        recyclerViewofhealth.setAdapter(adapter);
+        findNews();
         return v;
+
+    }
+
+    private void findNews() {
+
+        ApiUtilities.getApiInterFace().getCategoryNews(country, category, 100, api).enqueue(new Callback<mainNews>() {
+            @Override
+            public void onResponse(Call<mainNews> call, Response<mainNews> response) {
+                if(response.isSuccessful()) {
+                    modelClassArrayList.addAll(response.body().getArticles());
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<mainNews> call, Throwable t) {
+
+            }
+        });
 
     }
 }
